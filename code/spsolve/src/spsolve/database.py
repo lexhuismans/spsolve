c702cf16f12665b88e67134bd6301f6b94602f1e
Lex <l.j.huismans@student.tudelft.nl>
2021-03-16 12:08:26 +0100

    Restructuring folders

---
+++ code/spsolve/src/spsolve/database.py
@@ -0,0 +1,94 @@
+"""
+Database
+"""
+# MATERIAL PROPERTIES
+materialproperty = {
+    'GaAs':{
+        'm_e':.067, # conduction band effective mass (relative to electron mass)
+        'm_hh':.45, # heavy hole band effective mass
+        'm_lh':.087, # light hole band effective mass 
+        'epsilonStatic':12.90, #dielectric constant
+        'Eg':1.4223,#1.42 # (ev) band gap
+        'Ep':28.8, # (eV) k.p matrix element (used for non-parabolicity calculation (Vurgaftman2001)
+        'F':-1.94, # Kane parameter (used for non-parabolicity calculation (Vurgaftman2001)
+        'band_offset':0.65, # conduction band/valence band offset ratio for GaAs - AlGaAs heterojunctions
+        'm_e_alpha':5.3782e18, # conduction band non-parabolicity variable for linear relation (Nelson approach)
+        # Valence band constants 
+        'delta':0.28, # (eV) Spin split-off energy gap
+        # below used by aestimo_numpy_h
+        'GA1':6.8, #luttinger parameter
+        'GA2':1.9, #luttinger parameter
+        'GA3':2.73, #luttinger parameter
+        'C11':11.879, # (GPa) Elastic Constants
+        'C12':5.376, # (GPa) Elastic Constants
+        'a0':5.6533, # (A)Lattice constant
+        'Ac':-7.17, # (eV) deformation potentials (Van de Walle formalism)
+        'Av':1.16, # (eV) deformation potentials (Van de Walle formalism)
+        'B':-1.7, # (eV) shear deformation potential (Van de Walle formalism)
+        'TAUN0':0.1E-7,# Electron SRH life time
+        'TAUP0':0.1E-7,# Hole SRH life time
+        'mun0':0.1,# Electron Mobility in m2/V-s
+        'mup0':0.02,# Electron Mobility in m2/V-s
+        'Cn0':2.8e-31,# generation recombination model parameters [cm**6/s]
+        'Cp0':2.8e-32,# generation recombination model parameters [cm**6/s]
+        'BETAN':2.0,# Parameter in calculatation of the Field Dependant Mobility
+        'BETAP':1.0,# Parameter in calculatation of the Field Dependant Mobility
+        'VSATN':3e5,# Saturation Velocity of Electrons
+        'VSATP':6e5, # Saturation Velocity of Holes
+        'AVb_E':-6.92#Average Valence Band Energy or the absolute energy level
+    },
+    'AlAs':{
+        'm_e':.15,
+        'm_hh':.51,
+        'm_lh':.18,
+        'epsilonStatic':10.06,
+        'Eg':3.0,#2.980,
+        'Ep':21.1,
+        'F':-0.48,
+        'band_offset':0.53,
+        'm_e_alpha':0.0,
+        'GA1':3.45,
+        'GA2':0.68,
+        'GA3':1.29, 
+        'C11':11.879,
+        'C12':5.376,
+        'a0':5.66, 
+        'Ac':-5.64,
+        'Av':2.47,
+        'B':-1.5,
+        'delta':0.28,
+        'TAUN0':0.1E-6,
+        'TAUP0':0.1E-6,
+        'mun0':0.15,
+        'mup0':0.1,
+        'Cn0':2.8e-31,# generation recombination model parameters [cm**6/s]
+        'Cp0':2.8e-32,# generation recombination model parameters [cm**6/s]
+        'BETAN':2.0,
+        'BETAP':1.0,# Parameter in calculatation of the Field Dependant Mobility
+        'VSATN':3e5,# Saturation Velocity of Electrons
+        'VSATP':6e5, # Saturation Velocity of Holes
+        'AVb_E':-7.49#Average Valence Band Energy or the absolute energy level
+    }}
+
+# ALLOY PROPERTIES
+alloyproperty = {
+    'AlGaAs':{
+        'bowing_param':0.37,
+        'band_offset':0.65,
+        'm_e_alpha':5.3782e18,
+        'delta_bowing_param':0.0,
+        'a0_sub':5.6533,
+        'material1':'AlAs',
+        'material2':'GaAs',
+        'TAUN0':0.1E-6,
+        'TAUP0':0.1E-6,
+        'mun0':0.15,
+        'mup0':0.1,
+        'Cn0':2.8e-31,# generation recombination model parameters [cm**6/s]
+        'Cp0':2.8e-32,# generation recombination model parameters [cm**6/s]
+        'BETAN':2.0,
+        'BETAP':1.0,# Parameter in calculatation of the Field Dependant Mobility
+        'VSATN':3e5,# Saturation Velocity of Electrons
+        'VSATP':6e5 , # Saturation Velocity of Holes
+        'AVb_E':-2.1#Average Valence Band Energy or the absolute energy level
+    }}
---
+++ code/spsolve/src/spsolve/solver.py
@@ -0,0 +1,513 @@
+import math
+import sys
+import time 
+
+from collections import namedtuple
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from scipy.linalg import eig as eigs
+from scipy import optimize
+
+import kwant
+
+# Physical constants
+k_b = 8.617333262145e-5 # eV/K
+epsilon_0 = 0.055263494 # q_e/(V*nm)
+m_e = 9.10938 * 10**-31 # kg
+
+h_bar = 0.276042828 # eV s
+m_eff = 1.08 # m_e
+q_e = 1 # elementary charge
+
+Material = namedtuple('Material', ['doping', 'm_eff', 'epsilon', 'band_offset', 'L'])
+
+def fermi_dirac_int(E_F, E, T):
+    """
+    Computes the integral of the Fermi-Dirac distribution. (As is needed for
+    the number of electrons in a subband (n) with a two dimensional DOS)
+    """
+    if T == 0:
+        below_fermi = E<E_F
+        return (E_F-E)*below_fermi
+    else:
+        return k_b * T * np.log(1 + np.exp((E_F-E)/(k_b*T)))
+
+class StackedLayers:
+    def __init__(self, T, N, bound_left, bound_right, *layers):
+        self.layers = layers    # Layers (tuple)
+        self.T = T 
+        self.N = N
+        
+        self.N_layers = len(layers)
+        self.bound_left = bound_left
+        self.bound_right = bound_right
+        
+        self.L = 0              # Total length (nm)
+        self.L_hj = np.array([0])
+        for layer in layers:
+            self.L += layer.L
+            self.L_hj = np.append(self.L_hj, self.L)
+        
+        self.grid = np.linspace(0, self.L, num=self.N+2)[1:-1]
+        self.dl = self.grid[0]
+        
+        # PROPERTIES
+        self.epsilon = np.zeros(self.N)
+        self.m_eff = np.zeros(self.N)
+        self.doping = np.zeros(self.N)
+        self.band_offset = np.zeros(self.N)
+        for i in range(self.N_layers):
+            where = np.argwhere((self.grid >= self.L_hj[i]) * (self.grid <= self.L_hj[i+1]))
+            self.epsilon[where] = self.layers[i].epsilon
+            self.m_eff[where] = self.layers[i].m_eff
+            self.doping[where] = self.layers[i].doping
+            self.band_offset[where] = self.layers[i].band_offset
+        self.band_offset = self.band_offset - self.band_offset[0]
+        
+        self.rho = q_e*self.doping            # Charge distribution
+        self.n_e = np.zeros(self.N)           # Electron density
+        self.phi = np.zeros(self.N)           # Potential
+        self.psi = np.zeros((self.N, self.N)) # Wavefunctions
+        self.energies = np.zeros(self.N)      # Energies
+        self.band  = self.band_offset         # Conduction band
+        
+        self.DOS = self.m_eff/(math.pi*h_bar**2) # Density of States
+        
+        self.make_pois_matrix()
+        self.make_system()
+        
+    def make_pois_matrix(self):
+        """
+        Compute the matrix that is used for solving the Poisson equation. 
+        """
+        epsilon_full = np.concatenate(([self.epsilon[0]], self.epsilon, [self.epsilon[-1]])) # Include edges
+        epsilon_half = np.convolve(epsilon_full, [.5, .5], 'valid')
+        epsilon_imh = epsilon_half[0:-1]       # epsilon^i-1/2
+        epsilon_iph = epsilon_half[1::]        # epsilon^i+1/2
+
+        km1 = epsilon_imh
+        k = -(epsilon_imh + epsilon_iph)
+        kp1 = epsilon_iph
+        
+        prefac = -1/self.dl**2
+        
+        pois_matrix = prefac * (np.diag(km1[1::], k=-1) + np.diag(k, k=0) + np.diag(kp1[0:-1], k=1))
+        
+        if self.bound_left[0] == False:
+            pois_matrix[0, 1] += prefac * self.layers[0].epsilon
+        if self.bound_right[0] == False:
+            pois_matrix[-1, -2] += prefac * self.layers[-1].epsilon
+            
+        self.pois_matrix = pois_matrix
+        
+    def make_system(self):
+        """
+        Build a kwant system which is a one dimensional chain. 
+        template = kwant.continuum.discretize('k_x * A(x) * k_x + V(x)')
+        print(template)
+        """ 
+        m_eff_full = np.concatenate(([self.m_eff[0]], self.m_eff, [self.m_eff[-1]]))
+        m_eff_half = np.convolve(m_eff_full, [.5, .5], 'valid')
+        m_eff_imh = m_eff_half[0:-1]
+        m_eff_iph = m_eff_half[1::]
+        
+        #t = h_bar**2/(2*m_eff*dl**2)
+
+        lat = kwant.lattice.chain(self.dl)
+        syst = kwant.Builder()
+        # ONSITE
+        def onsite(site, pot):
+            i = site.tag
+            t = h_bar**2/(2*self.dl**2) * (1/m_eff_imh[i] + 1/m_eff_iph[i])
+            return t + pot[i]
+
+        syst[(lat(x) for x in range(int(self.N)))] = onsite
+        # HOPPING
+        for i in np.arange(self.N-1)+1:
+            t = h_bar**2/(2*m_eff_imh[i]*self.dl**2)
+            syst[lat(i), lat(i-1)] = -t
+
+        # ATTACH LEADS
+        left_lead = kwant.Builder(kwant.TranslationalSymmetry((-self.dl,)))
+        t = h_bar**2/(2*m_eff_imh[0]*self.dl**2)
+        left_lead[lat(0)] = 2*t
+        left_lead[lat.neighbors()] = -t
+        syst.attach_lead(left_lead)
+        
+        right_lead = kwant.Builder(kwant.TranslationalSymmetry((self.dl,)))
+        t = h_bar**2/(2*m_eff_iph[-1]*self.dl**2)
+        right_lead[lat(0)] = 2*t
+        right_lead[lat.neighbors()] = -t
+        syst.attach_lead(right_lead)
+        
+        self.syst = syst.finalized()
+    
+    def solve_charge(self):
+        """
+        Solve for the charge distribution. 
+        """
+        inner_product = self.psi**2
+        N_elec = self.DOS*fermi_dirac_int(0, self.energies, self.T)
+        
+        self.n_e = np.dot(inner_product, N_elec)
+        self.rho = -q_e*self.n_e+q_e*self.doping
+    
+    def solve_poisson(self):
+        """
+        Solve the Poisson equation non-uniform mesh.
+        """
+        adjusted_rho = self.rho.copy()
+
+        #--------------------BOUNDARIES----------------------
+        if self.bound_left[0]:
+            # Dirichlet
+            adjusted_rho[0] += self.layers[0].epsilon * self.bound_left[1] / self.dl**2
+        else:
+            # Neumann
+            adjusted_rho[0] += -2*self.bound_left[1]*self.layers[0].epsilon / self.dl 
+            
+        if self.bound_right[0]:
+            # Dirichlet
+            adjusted_rho[-1] += self.layers[-1].epsilon * self.bound_right[1] / self.dl**2
+        else:
+            # Neumann
+            adjusted_rho[-1] += 2*self.bound_right[1]*self.layers[-1].epsilon / self.dl
+        #---------------------SOLVE--------------------------
+        self.phi = np.linalg.solve(self.pois_matrix, adjusted_rho)
+        self.band = q_e*self.phi + self.band_offset
+    
+    def solve_schrodinger(self):
+        """
+        Gives the wavefunctions for a given potential distribution. 
+        """
+        pot_energy = self.band
+        ham = self.syst.hamiltonian_submatrix(sparse = False, params=dict(pot=pot_energy))
+        
+        energies, psi = eigs(ham)
+        
+        # Sort from lowest to highest energy
+        sort_index = np.argsort(energies)
+        
+        energies = energies[sort_index]
+        psi = np.real(psi[:, sort_index])/math.sqrt(self.dl) # Every column is a wavefunction
+        
+        self.energies = np.real(energies)
+        self.psi = psi
+        #tf = time.time()
+        #print('    Final: ', tf-t0)
+    
+    def solve_optimize(self):
+        def self_consistent(phi):
+            self.phi = phi
+            phi_old = phi.copy()
+            
+            self.solve_schrodinger()
+            self.solve_charge()
+            self.solve_poisson()
+            
+            # Compute error
+            diff = np.real(self.phi-phi_old)
+            return diff
+        
+        self.solve_poisson()
+        
+        optim_result = optimize.root(self_consistent, self.phi, method = 'anderson', options=dict(maxiter=3))
+        # options=dict(maxiter=1000)
+        
+    def solve_pred_corr(self):
+        self.solve_poisson()
+        self.solve_schrodinger()
+        
+        def jac(phi):
+            exp = np.exp(-self.energies+q_e*(phi-self.phi))
+            return q_e * exp/(1+exp)
+        
+        def pred_charge(phi):
+            inner_product = self.psi**2
+            delta_pot = q_e*(phi - self.phi) 
+            N_elec = self.DOS*fermi_dirac_int(0, self.energies-delta_pot, self.T)
+            predictor = np.dot(inner_product, N_elec)
+            return predictor
+        
+        def solve_newt(phi):
+            return np.dot(self.pois_matrix, phi) - q_e*(pred_charge(phi) - self.doping)
+        
+        error = 1
+        tol = 1e-4
+        max_iter = 1000
+        i = 0
+        while error > tol and i < max_iter:
+            rho_old = self.rho
+            self.phi = optimize.newton(solve_newt, self.phi, fprime = jac)
+            self.solve_schrodinger()
+            self.solve_charge()
+
+            error = np.abs(np.dot(rho_old, self.rho))
+            print(error)
+            i = i + 1
+        
+#-----------------------Non Class Code---------------------------
+    
+def comp_schrod_matrix(phi, delta_l, m_eff = 1.08):
+    """
+    Builds the matrix for solving the Schrodinger equation.
+
+    Parameters
+    ----------
+    phi : float
+        vector phi containing potential values for each gridpoint.
+    delta_l : float
+        distance between each gridpoint.
+    m_eff : float
+        effective mass of electron in m_e. 
+
+    Returns
+    -------
+    schrod_matrix : float
+        numpy matrix used in Schrodinger equation for solving wavefunction.
+    """
+    N = phi.size
+    pre_fac = h_bar**2/(2*m_eff*delta_l**2)
+    """
+    schrod_matrix = (np.eye(N, k=-1)+np.eye(N, k=1))
+    np.fill_diagonal(schrod_matrix,((q_e*phi/pre_fac-2)))
+    schrod_matrix = -pre_fac*schrod_matrix
+    """
+    schrod_matrix = (np.eye(N, k=-1)+np.eye(N, k=1)) - 2*np.eye(N)
+    schrod_matrix = -pre_fac*schrod_matrix
+    
+    potential_matrix = np.zeros((N,N))
+    diagonal = phi + np.append(0, phi[0:-1])
+    np.fill_diagonal(potential_matrix, diagonal)
+    potential_matrix = potential_matrix + np.eye(N, k=1)*np.append(0, -phi[0:-1]) + np.eye(N, k=-1)*np.append(-phi[0:-1], 0)
+    
+    schrod_matrix = schrod_matrix + potential_matrix
+    return schrod_matrix
+
+def make_system(dl, L, m_eff):
+    """
+    Build a kwant system which is a one dimensional chain. 
+    
+    Parameters
+    ----------
+    dl : float
+        distance between each gridpoint.
+    L : int
+        length of system in nm. 
+    m_eff : float
+        effective mass of electron in m_e. 
+        
+    Returns
+    -------
+    syst : kwant.Builder
+        finalized system (kwant). 
+    """
+    t = h_bar**2/(2*m_eff*dl**2)
+    
+    lat = kwant.lattice.chain(dl)
+    syst = kwant.Builder()
+    
+    def onsite(site, pot):
+        return 2*t - q_e*pot[site.tag]
+    
+    syst[(lat(x) for x in range(int(L)))] = onsite
+    syst[lat.neighbors()] = -t
+    
+    # Attach leads
+    lead = kwant.Builder(kwant.TranslationalSymmetry((-dl,)))
+    lead[lat(0)] = 2*t
+    lead[lat.neighbors()] = -t
+    syst.attach_lead(lead)
+    syst.attach_lead(lead.reversed())
+    
+    return syst.finalized()
+
+def comp_pois_matrix(dl, N, bound_one_dirich = True, bound_two_dirich = True):
+    """
+    Builds the matrix for solving the Poisson equation
+
+    Parameters
+    ----------
+    dl : float
+        distance between each gridpoint.
+    N : int
+        number of gridpoints.
+    bound_one_dirich : boolean
+        True if first boundary is Dirichlet, False for Neumann.
+    bound_two_dirich : boolean
+        True if second boundary is Dirichlet, False for Neumann.
+
+    Returns
+    -------
+    pois_matrix : float
+        numpy matrix used in Poisson equation for solving potential.
+    """
+    pois_matrix = (np.eye(N,k=-1) + np.eye(N,k=1) - 2*np.eye(N))
+
+    # Change according to boundary conditions
+    # First boundary
+    if bound_one_dirich:
+        pois_matrix[0,0:2] = [1, 0]
+    else:
+        pois_matrix[0, 0:2] = [-2, 2]
+    # Second boundary
+    if bound_two_dirich:
+        pois_matrix[-1, -2:] = [0, 1]
+    else:
+        pois_matrix[-1, -2:] = [2, -2]
+
+    pois_matrix = -epsilon_0/dl**2 * pois_matrix
+    return pois_matrix
+
+def solve_poisson(pois_matrix, rho, dl, alpha, beta):
+    """
+    Solves the discrete Poisson equation.
+
+    Parameters
+    ----------
+    pois_matrix : float
+        Poisson matrix as given by comp_pois_matrix
+    rho : float
+        charge distribution vector
+    dl : float
+        distance between gridpoints
+    alpha : float
+        left boundary value
+    beta : float
+        right boundary value
+
+    Returns
+    -------
+    phi : float
+        potential vector
+    """
+    adjusted_rho = rho.copy()
+    # Adjust charge distribution according to boundary condition
+    # First boundary
+    if pois_matrix[0,1] == 0:
+        adjusted_rho[0] = -alpha*epsilon_0/dl**2
+    else:
+        adjusted_rho[0] = adjusted_rho[0] + 2*alpha*epsilon_0/dl
+    # Second boundary
+    if pois_matrix[-1, -2] == 0:
+        adjusted_rho[-1] = -beta*epsilon_0/dl**2
+    else:
+        adjusted_rho[-1] = adjusted_rho[-1] - 2*beta*epsilon_0/dl
+
+    # Solve equation
+    phi = np.linalg.solve(pois_matrix, adjusted_rho)
+    return phi
+
+def solve_schrod(phi, dl):
+    """
+    Solves the discrete Schrodinger equation.
+
+    Parameters
+    ----------
+    phi : float
+        potential vector.
+    dl : float 
+        distance between gridpoints.
+
+    Returns
+    -------
+    energies : float
+        eigen energies of the solution (eV).
+    psi : float
+        wavefunctions (eigenvectors of the solution). 2D array with wavefunction for each i, psi[:,i].
+    """
+    schrod_matrix = comp_schrod_matrix(phi, dl)
+    energies, psi = np.linalg.eig(schrod_matrix)
+
+    # Sort from lowest to highest energy
+    sort_index = np.argsort(energies)
+    energies = energies[sort_index]
+    psi = psi[:, sort_index]/math.sqrt(dl) # Every column is a wavefunction
+    return energies, psi
+
+def solve_syst(syst, phi, dl):
+    """
+    Gives the wavefunctions for a given potential distribution. 
+    
+    Parameters
+    ----------
+    syst : kwant.Builder.finalized
+        finalized system. 
+    phi : float
+        array with potential for each gridpoint. 
+    dl : float 
+        distance between gridpoints.
+        
+    Returns
+    -------
+    energies : float 
+        energies of all the wavefunctions. 
+    psi : float 
+        2D array with wavefunction for each i, psi[:, i]. 
+    """
+    V = q_e*phi
+    ham = syst.hamiltonian_submatrix(sparse = False, params=dict(pot=V))
+    #print('Hamiltonian: \n', ham)
+    #t0 = time.time()
+    energies, psi = eigs(ham)
+    #t1 = time.time()
+    #print('Solve ham: ', t1-t0)
+    #print('Energies: \n', energies)
+    
+    # Sort from lowest to highest energy
+    sort_index = np.argsort(energies)
+    energies = energies[sort_index]
+    psi = np.real(psi[:, sort_index])/math.sqrt(dl) # Every column is a wavefunction
+    
+    return np.real(energies), psi
+
+def solve_charge_dist(psi, energies, Ef, T=0, N_D = None):
+    """
+    Solve the for the charge distribution given the wavefunctions.
+
+    Parameters
+    ----------
+    psi : float
+        2D array with wavefunction for each i, psi[:,i].
+    energies : float 
+        1D array with the energies corresponding to the wavefunction psi[:, i].
+    Ef : float 
+        Fermi energy. 
+    T : float
+        temperature (Kelvin).
+    N_D : float
+        1D array with doping concentrations. 
+
+    Returns
+    -------
+    rho : float
+        1D array with charge distribution.
+    """
+    DOS = m_eff/(math.pi*h_bar**2) # Density of States
+    if T == 0:
+        # Remove wavefunctions with energy above Fermi energy.
+        psi = psi[:,np.argwhere(energies < Ef)]
+        energies = energies[np.argwhere(energies < Ef)]
+        
+        # Compute charge density
+        inner_product = np.squeeze(psi**2, axis = 2)
+        N_elec = DOS*(Ef-energies)
+        
+        rho = -q_e*np.dot(inner_product, N_elec)
+        
+        # Add doping
+        if not isinstance(N_D, type(None)): 
+            rho = rho + q_e * N_D
+    else:
+        inner_product = psi**2
+        N_elec = DOS*fermi_dirac_int(Ef, energies, T)
+        rho = -q_e*np.dot(inner_product, N_elec)
+        
+        # Add doping
+        if not isinstance(N_D, type(None)): 
+            rho = rho + q_e * N_D
+     
+    return rho
+    
